66fbe195246ef959312df82637e00b535145588d
Orock237 <81926418+Orock237@users.noreply.github.com>
2024-11-03 21:41:43 +0100

    Add database login and spring boot security

--- App/city-feedback/src/main/java/com/example/city_feedback/domain/User.java
+++ App/city-feedback/src/main/java/com/example/city_feedback/domain/User.java
@@ -1,7 +1,15 @@
 package com.example.city_feedback.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
 public class User {
-    public int id;
+    @Id
+    @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
+    public long id;
+
     public String firstName;
     public String lastName;
     public String email;
--- App/city-feedback/src/main/java/com/example/city_feedback/services/AuthenticationService.java
+++ App/city-feedback/src/main/java/com/example/city_feedback/services/AuthenticationService.java
@@ -1,4 +1,8 @@
 package com.example.city_feedback.services;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class AuthenticationService {
+    
 }
