2da57207b93d817f36da32ecfa1302f87eee6b28
Orock237 <81926418+Orock237@users.noreply.github.com>
2024-11-09 17:49:48 +0100

    resolve conflicts

--- App/city-feedback/src/main/java/com/example/city_feedback/domain/Complaint.java
+++ App/city-feedback/src/main/java/com/example/city_feedback/domain/Complaint.java
@@ -1,60 +1,8 @@
 package com.example.city_feedback.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-
-@Entity
-@Table(name = "COMPLAINTS")
 public class Complaint {
-    @Id
-    @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
     private int id;
-
     private String title;
     private String description;
-    private String  progress;
-//    private ComplaintProgress progress;
-
-    public Complaint() {}
-
-    public Complaint(String title, String description, String progress) {
-        this.title = title;
-        this.description = description;
-        this.progress = progress;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getProgress() {
-        return progress;
-    }
-
-    public void setProgress(String progress) {
-        this.progress = progress;
-    }
-
+    private ComplaintProgress progress;
 }
